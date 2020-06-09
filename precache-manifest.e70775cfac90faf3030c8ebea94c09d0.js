c6acbbe79fe3d68cc2d579b52de05078c7c44a89
Egor Stronhin <i@egor.xyz>
2020-06-09 16:35:38 +0300

    Updates

---
+++ precache-manifest.e70775cfac90faf3030c8ebea94c09d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38ee5ee604c2d2bc478176bfbc62d9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164459c6cd67e5a45b96",
+    "url": "/static/css/2.01e1cb60.chunk.css"
+  },
+  {
+    "revision": "a26cf0eda19408f51747",
+    "url": "/static/css/main.4e6e0edb.chunk.css"
+  },
+  {
+    "revision": "164459c6cd67e5a45b96",
+    "url": "/static/js/2.453a3139.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/static/js/2.453a3139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26cf0eda19408f51747",
+    "url": "/static/js/main.36cfd182.chunk.js"
+  },
+  {
+    "revision": "93e232d72defdeddd03e",
+    "url": "/static/js/runtime-main.7d2d155b.js"
+  },
+  {
+    "revision": "f195fec1db34d945845a97227d1273d8",
+    "url": "/static/media/logo.f195fec1.svg"
+  }
+]);
